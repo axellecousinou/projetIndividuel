fe034ea05d59de6efe23db9d077f5d6eba315dc3
Axelle Cousinou <axelle.cousinou@gmail.com>
2021-05-20 12:48:15 +0200

    Création de la page d'affichage des posts d'une communauté et des détails des posts

--- projet/communitymanager/urls.py
+++ projet/communitymanager/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.log),
     path('desabonnement/<int:com_id>', views.desabonnement, name='desabonnement'),
     path('abonnement/<int:com_id>', views.abonnement, name='abonnement'),
+    path('communaute/<int:com_id>', views.communaute, name='communaute'),
+    path('post-details/<int:post_id>', views.post_details, name='details'),
 ]
--- projet/communitymanager/views.py
+++ projet/communitymanager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Communaute
+from .models import Post
 
 
 def log(request):
@@ -31,3 +32,15 @@ def abonnement(request, com_id):
     c = Communaute.objects.get(id=com_id)
     c.abonnes.add(request.user)
     return redirect('communautes')
+
+
+def communaute(request, com_id):
+    list = Post.objects.all()
+    com_name = Communaute.objects.get(id=com_id).nom
+    return render(request, 'communitymanager/communaute.html', locals())
+
+
+def post_details(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'communitymanager/post-details.html', locals())
+
